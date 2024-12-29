6806624abcf667e50fc50fd72f0ef2ce4d16cb51
mohamedkhayat <mohamekhayat025@gmail.com>
2024-12-29 20:43:36 +0100

    added GPU support

---
+++ DeviceSelector.py
@@ -0,0 +1,24 @@
+import os
+import numpy
+
+_GPU_AVAILABLE = False
+
+try:
+  output = os.popen('nvidia-smi').read()
+  if "NVIDIA-SMI" in output:
+    _GPU_AVAILABLE = True
+    import cupy
+    np = cupy
+    
+  else:
+    np = cupy
+    
+except Exception as e:
+  np = numpy
+  raise ValueError(f"Exception {e}")
+
+def get_numpy():
+  return np
+
+def is_gpu_available():
+  return _GPU_AVAILABLE
