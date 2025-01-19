fixed bugs with visualization and made loss/accuracy calculation consistent

Network.py
@@ -1,6 +1,6 @@
 import time
-np = get_numpy()
 from DeviceSelector import *
+np = get_numpy()
 from EarlyStopping import EarlyStopping
 from utils import create_mini_batches
 from Layers import Dropout,Dense
@@ -31,10 +31,10 @@ class NeuralNetwork():
       
     output = X
     for layer in self.layers:
-      
+      """
       if self.training == False and isinstance(layer,Dropout):
         continue
-      
+      """
       output = layer.forward(output,train)
 
     return output
@@ -102,9 +102,12 @@ class NeuralNetwork():
         y_pred = self.forward(X_batch, train=True)      
         
         loss = self.criterion(y_batch, y_pred)
-        epoch_loss += loss
+        epoch_loss += float(loss)
         
         dA = self.criterion.backward(y_batch, y_pred)
+
+        #print("Y pred before argmax : ",y_pred.shape)
+        #print("Y batch before argmax : ",y_batch.shape)
 
         if isinstance(self.criterion, BCELoss):
           y_pred_labels = (y_pred > 0.5).astype(int)
@@ -114,15 +117,14 @@ class NeuralNetwork():
           
         elif(isinstance(self.criterion, CrossEntropyLoss)):
           y_pred_labels = np.argmax(y_pred, axis = 0)
-          y_true_labels = np.argmax(y_batch, axis=0)  
-          
+          y_true_labels = np.argmax(y_batch, axis = 0)  
           batch_correct = np.sum(y_pred_labels == y_true_labels)
           
           self.backprop(y_batch)
         
         self.optimize()
         
-        correct_predictions += batch_correct
+        correct_predictions += int(batch_correct)
         total_samples += y_batch.shape[1]
         num_batches += 1
         
@@ -133,15 +135,54 @@ class NeuralNetwork():
       train_accuracies.append(float(avg_train_accuracy))
 
       if validation_data is not None:
+        X_test, y_test = validation_data
+        self.eval()
         
+        # Create batches for test data too
+        test_batches = create_mini_batches(X_test, y_test, batch_size=batch_size,
+                                        shuffle=False, drop_last=False)
+        
+        test_loss = 0.0
+        test_correct = 0
+        test_total = 0
+        num_batches = 0
+        
+        for X_batch_test, y_batch_test in test_batches:
+            y_pred_test = self.forward(X_batch_test, train=False)
+            batch_test_loss = self.criterion(y_batch_test, y_pred_test)
+            test_loss += float(batch_test_loss)
+            
+            
+            if isinstance(self.criterion, BCELoss):
+              y_pred_test_labels = (y_pred_test > 0.5).astype(int)
+              test_correct += np.sum(y_pred_test_labels == y_batch_test)
+              
+            elif(isinstance(self.criterion, CrossEntropyLoss)):
+              y_pred_test_labels = np.argmax(y_pred_test, axis=0)
+              y_true_test = np.argmax(y_batch_test, axis=0)
+              test_correct += np.sum(y_pred_test_labels == y_true_test)
+            
+            test_total += y_batch_test.shape[1]
+
+            total_samples += y_batch_test.shape[1]
+            num_batches += 1
+        
+        test_loss = test_loss / num_batches  # Average loss over batches
+        test_accuracy = test_correct / test_total
+        
+        test_losses.append(float(test_loss))
+        test_accuracies.append(float(test_accuracy))
+        """
+        self.eval()
         X_test, y_test = validation_data
         
         y_pred_test = self.forward(X_test, train = False)
         
         test_loss = self.criterion(y_test, y_pred_test)
-        test_losses.append(test_loss.tolist())
+        test_losses.append(float(test_loss))
         
-        
+        print("Y test before argmax : ",y_test.shape)
+        print("Y pred test before argmax : ",y_pred_test.shape)
         if isinstance(self.criterion, BCELoss):
           y_pred_test_labels = (y_pred_test > 0.5).astype(int)
           test_correct = np.sum(y_pred_test_labels == y_test)
@@ -152,11 +193,12 @@ class NeuralNetwork():
           
           test_correct = np.sum(y_test_labels == y_pred_test_labels)
         
-        test_accuracy = test_correct / y_test.shape[1] 
-        test_accuracies.append(float(test_accuracy))
-     
+        test_accuracy = float(test_correct / y_test.shape[1]) 
+        test_accuracies.append(test_accuracy)
+      """
       if epoch % 10 == 0:
-        print(f"Epoch : {epoch} : Loss : {float(test_loss):.4f}")
+        print(f"Epoch : {epoch}")
+        print(f"Train Loss : {float(avg_train_loss):.4f} Test Loss : {float(test_loss):.4f}")
 
       if early_stopping_patience is not None and er(test_loss):
         break
@@ -218,14 +260,12 @@ class NeuralNetwork():
       return predictions
   
   def accuracy_score(self,y_pred,y_true):
-    
+    batch_size = y_true.shape[1]
+
     if isinstance(self.criterion, CrossEntropyLoss):
-      y_pred = np.argmax(y_pred, axis = 0)
+      #y_pred = np.argmax(y_pred, axis = 0)
       y_true= np.argmax(y_true, axis = 0)
 
-    elif isinstance(self.criterion, BCELoss):
-      y_pred = (y_pred > 0.5)
-
     correct = np.sum(y_pred == y_true)
-    return correct / y_true.shape[1]
+    return float(correct / batch_size)
 

main.py
@@ -1,6 +1,6 @@
+from DeviceSelector import *
 np = get_numpy()
 from utils import *
-from DeviceSelector import *
 from Network import NeuralNetwork
 from Losses import BCELoss,CrossEntropyLoss
 
@@ -12,33 +12,24 @@ _GPU_AVAILABLE = is_gpu_available()
 # Setting random seed for reproducibility
 
 np.random.seed(42)
-
+problem = 2
 # Loading Mnist data
 if __name__ == "__main__":
+  print("loading data")
   try:
-    X, y = load_binary_mnist()
-    
-  except:
-    
+    if problem == 2:
+      X, y = load_mnist()
+    else:
+      X, y = load_binary_mnist()
+  except Exception as e:
+    print(e)
     #Falling back to generating XOR in case of errors
-    
+    print(f"Falling back to xor")
     n_samples = 2000
     X, y = generate_xor_data(n_samples, np)
   
-  """
-  y = y.flatten()
-  n_samples = len(y)
-  # Initialize the output array
-  #one_hot = np.zeros((2, n_samples))
-  
-  # Set the appropriate indices to 1
-  one_hot[0, y == 0] = 1  # First row for class 0
-  one_hot[1, y == 1] = 1  # Second row for class 1 
-  y = one_hot
-  """
   n_features = X.shape[0]
   n_classes = y.shape[0]
-
   # specifying percantage of data to be used for validation
 
   ratio = 0.2
@@ -53,26 +44,30 @@ if __name__ == "__main__":
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = ratio)
 
   # How Big/Small your weight updates are, essentially, how fast your model learns 
-  learning_rate = 0.3
+  learning_rate = 0.5
   
   # Initializing our Loss function, We use BCELoss because its a binary classification problem
-  loss = BCELoss()
-  #loss =  CrossEntropyLoss()
+  if problem == 1:
+    loss = BCELoss()
+    final_layer = Sigmoid()
+  else:
+    loss = CrossEntropyLoss()
+    final_layer = Softmax()
 
   layers = [
     Dense(input_size = n_features, output_size = 64, initializer = 'he'), # Input layer, input size = n_features, output_size (n of units) = 64, HE init because it uses ReLU
     ReLU(), # ReLU Activation Function
     Dense(input_size = 64, output_size = 64, initializer = 'he'), # First hidden layer, input size = 64, output size = 64, he init too because it uses ReLU
     ReLU(), # ReLU again
-    Dropout(keep_prob = 0.8), # Dropout layer, turns off 10% of units
+    Dropout(keep_prob = 0.95), # Dropout layer, turns off 10% of units
     Dense(input_size = 64, output_size = 32, initializer = 'he'), # Second Hidden layer, input size = 64, output size = 32, he init again because it uses ReLU
     ReLU(), # relu again
     Dense(input_size = 32, output_size = 32, initializer = 'he'), # Third Hidden layer input size = 32, output size = 32 he init again
     ReLU(), # relu again
-    Dropout(keep_prob = 0.8), # Dropout layer, turns off 10% of units
+    Dropout(keep_prob = 0.95), # Dropout layer, turns off 10% of units
     Dense(input_size = 32, output_size = n_classes, initializer = 'glorot'), # Output layer, input size = 32, output size = n_classes (1), glorot init because it uses sigmoid
-    Sigmoid() # Sigmoid Activation function because we are using BCELoss (it's a binary classification problem, predicting if an image is 1 or not 1)
-    #Softmax() # Sigmoid Activation function because we are using BCELoss (it's a binary classification problem, predicting if an image is 1 or not 1)
+    final_layer# Sigmoid Activation function because we are using BCELoss (it's a binary classification problem, predicting if an image is 1 or not 1)
+     # Sigmoid Activation function because we are using BCELoss (it's a binary classification problem, predicting if an image is 1 or not 1)
   ]
 
   model = NeuralNetwork(n_classes = n_classes , # Needed
@@ -82,20 +77,21 @@ if __name__ == "__main__":
                         )
 
   # Training the model for 100 epochs
+  print("starting training")
 
   History = model.fit(X_train = X_train, # Needed
                       y_train = y_train, # Needed
                       batch_size = 64*8, # Optional, defaults to 64
                       shuffle = True, # Optional, defaults to True
-                      epochs = 100, # Needed
+                      epochs = 200, # Needed
                       validation_data = (X_test, y_test), # Optional if you dont need plotting
-                      early_stopping_patience = None, #15, # Optional
+                      early_stopping_patience = 15, #15, # Optional
                       early_stopping_delta = 0.001 # Optional
             )
 
   # Print Time Elapsed and Device used to train
 
-  print(f"Time Elapsed : {History['Time_Elapsed']:.2F} seconds on : {'GPU' if _GPU_AVAILABLE else 'CPU'}")
+  print(f"\nTime Elapsed : {History['Time_Elapsed']:.2F} seconds on : {'GPU' if _GPU_AVAILABLE else 'CPU'}\n")
 
   plot_metrics(History)
 
@@ -118,4 +114,4 @@ if __name__ == "__main__":
   print(f"Test Accuracy : {float(test_accuracy):.4f}")
 
   #Plotting random n_images from the test set with their predictions
-  plot_image(X = X_test, model = model, n_images = 6, original_image_shape = (28, 28))
+  plot_image(X = X_test, model = model, n_images = 6, original_image_shape = (28, 28), n_classes = n_classes)